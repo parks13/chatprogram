54e007da2fbfe62618ac8efadc3f0892663f9d13
Jason Sungmin Park <parks13@spu.edu>
2019-05-18 14:00:28 -0700

    Added basic client file to run UI

---
+++ client.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtCore, uic, QtWidgets
+
+import sys
+
+UIClass, QtBaseClass = uic.loadUiType("mainwindow.ui")
+
+class MyApp(UIClass, QtBaseClass):
+    def __init__(self):
+        UIClass.__init__(self)
+        QtBaseClass.__init__(self)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
